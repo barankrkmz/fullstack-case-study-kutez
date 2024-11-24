e5c42051f77c07dbde3a24608cc2ebbc12615560
Baran Korkmaz <131336066+barankrkmz@users.noreply.github.com>
2024-11-24 03:43:37 +0300

    Add files via upload

---
+++ fullstack-case-frontend/src/App.js
@@ -0,0 +1,13 @@
+import ProductList from './components/ProductList';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <ProductList />  {/* Ürün listesini render eden bileşen */}
+    </div>
+  );
+}
+
+export default App;
